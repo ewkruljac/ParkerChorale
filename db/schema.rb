0c98c505a5a059d875c95debc1eea3f06473ba76
Edward Kruljac <ewkruljac@gmail.com>
2018-06-28 20:44:03 -0600

    Created Event resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_030904) do
+ActiveRecord::Schema.define(version: 2018_06_29_024300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2018_06_27_030904) do
     t.string "name"
     t.string "role"
     t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "ticket_url"
+    t.string "you_tube_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
